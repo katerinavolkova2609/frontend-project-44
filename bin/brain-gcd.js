87988f54c884e58024c9833f5f8d1525e8b5c132
Ekaterina Volkova <katerinavolkova2609@gmail.com>
2023-12-26 11:39:25 +0300

    add brain-gcd.js

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import { findCommonDivisor } from "../src/games/brain-gcd.js";
+
+findCommonDivisor();
---
+++ src/games/brain-gcd.js
@@ -0,0 +1,34 @@
+import { gameSession } from "../index.js";
+
+const findCommonDivisor = () => {
+
+const rules = () =>
+  console.log("Find the greatest common divisor of given numbers.");
+
+const getCollOfNumbers = () => {
+  const collOfNumbers = [];
+  const commonDivisors = [];
+  for (let i = 0; i <= 2; i += 1) {
+    let num1 = Math.floor(Math.random() * (100 - 1) + 1);
+    let num2 = Math.floor(Math.random() * (100 - 1) + 1);
+    let question = num1 + " " + num2;
+    for (let j = 1; j <= 100; j += 1) {
+      let divisor;
+      if (num1 % j === 0 && num2 % j === 0) {
+        divisor = j;
+        commonDivisors.push(divisor);
+      }
+    }
+    let correctAnswer = commonDivisors[commonDivisors.length - 1].toString();
+    collOfNumbers[i] = [];
+    collOfNumbers[i].push(question, correctAnswer);
+  }
+  return collOfNumbers;
+};
+
+let collOfNumbers = getCollOfNumbers();
+gameSession(rules, collOfNumbers);
+
+};
+
+export { findCommonDivisor };
