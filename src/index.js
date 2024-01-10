d4db5f0dc117ca8ac3752e2e1ee5cf773d9ba590
Ekaterina Volkova <katerinavolkova2609@gmail.com>
2024-01-10 17:21:53 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 const gameSession = (rules, collOfNumbers) => {
-  console.log("Welcome to the Brain Games!");
-  const name = readlineSync.question("May I have your name? ");
-  console.log("Hello, " + name + "!");
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
   console.log(rules);
   for (let i = 0; i <= 2; i += 1) {
     const [question, correctAnswer] = collOfNumbers[i];
-    console.log("Question: " + question);
-    const answer = readlineSync.question("Your answer: ");
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Your answer: ');
     if (correctAnswer === answer) {
-      console.log("Correct!");
-    } else
-      return console.log(
-        `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`
-      );
+      console.log('Correct!');
+    } else {
+      return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
+    }
   }
   return console.log(`Congratulations, ${name}!`);
 };
 
-export { gameSession };
+export default gameSession;
